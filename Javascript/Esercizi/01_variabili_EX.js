dd86156eb73e4d57d2d203dc625da917d99ababd
Nico Ponziani <n.ponziani@almaviva.it>
2025-02-18 14:35:00 +0100

    Esercizio dispensa 1 e 2

---
+++ Javascript/Esercizi/01_variabili_EX.js
@@ -0,0 +1,23 @@
+/**
+ * Esercizio 1
+ * Dichiara ed inizializza una variabile per ogni tipologia di dato che abbiamo in javascript
+ * (string, number, boolean, array, object, null, undefined)
+ */
+
+// CANCELLAMI E RISCRIVI IL CODICE QUI SOTTO
+
+/**
+ * Esercizio 2
+ * Dichiara una variabile (SOLO DICHIARAZIONE);
+ * Assegnagli un valore di tipo string e stampala in console.
+ * Poi assegna a quella stessa variabile un valore di tipo number e stampala in console.
+ */
+
+// CANCELLAMI E RISCRIVI IL CODICE QUI SOTTO
+
+/**
+ * Esercizio 3
+ * Dichiara ed inizializza una costante;
+ */
+
+// CANCELLAMI E RISCRIVI IL CODICE QUI SOTTO
---
+++ Javascript/Esercizi/02_operatori_EX.js
@@ -0,0 +1,87 @@
+/**
+ * Esercizio 1
+ * Dichiara ed inizializza una variabile di tipo numerico con valore 10.
+ * Dichiara ed inizializza una variabile di tipo stringa con valore "Ciao Mondo!".
+ */
+
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 2
+ * Usa la variabile numerica di prima ed aggiungi 1 usando l'operatore di incremento
+ */
+
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 3
+ * Somma la variabile numerica di prima a 9 e salva il risultato in una nuova variabile.
+ * Stampa entrambe le variabili.
+ */
+
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 4
+ * Dividi la variabile numerica che contiene il risultato di prima per 5 e salva il risultato nella stessa variabile.
+ * Infine stampala.
+ */
+
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 5
+ * Concatena tramite l'operatore + la variabile stringa del primo esercizio con la stringa " Pinguino".
+ * Stampa il risultato senza assegnare il valore ad una variabile.
+ */
+
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 6
+ * Concatena alla variabile sotto dichiarata la variabile numerica del primo esercizio
+ * e stampa il risultato.
+ */
+
+let stringa = "Il numero è: ";
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 7
+ * Stampa in console il risultato del confronto tra le due variabili dichiarate sotto (considerando anche il tipo di dato)
+ */
+let nome = "Kappa";
+let cognome = "Ti";
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 8
+ * Stampa in console il risultato del confronto tra le due variabili dichiarate sotto (non considerando anche il tipo di dato)
+ */
+let numero = 10;
+let numero2 = "10";
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 9
+ * Decrementa la prima variabile numerica qui sotto di 1 tramite l'operatore di decremento
+ * Incremento la seconda variabile numerica qui sotto di 1 tramite l'operatore di incremento
+ * Confronta le variabili e vedi se la prima è minore o uguale la seconda ed assegna il risultato ad un'altra variabile
+ * Stampa il risultato
+ *
+ */
+let k = 10;
+let n = 10;
+//CANCELLAMI E SCRIVI IL CODICE SOTTO
+
+/**
+ * Esercizio 10
+ * Considerando le due variabili che hai sopra stampa in console le seguenti operazioni:
+ * -La somma delle due variabili
+ * -La differenza tra le due variabili
+ * -Il resto delle due variabili
+ * -il confronto stretto (===) di uguaglianza
+ * -il confronto stretto di disuguaglianza
+ * -il confronto di maggiore o uguale
+ * -il confronto di minore o uguale
+ */
