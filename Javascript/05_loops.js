4a722d53257cb073e20981fd667c4802ed2bb4ae
Nico Ponziani <n.ponziani@almaviva.it>
2025-03-06 17:33:34 +0100

    Classi e oggetti

--- Javascript/05_loops.js
+++ Javascript/05_loops.js
@@ -1,14 +1,128 @@
 /**
  * LOOPS
- * Cosa si intende con loops? il loops risponde alla necessità di eseguire un blocco di codice piu volte in base al numero di iterazioni
- * che sono necessarie.
- * Ci sono molti casi in cui abbiamo la necessità di eseguire una porzione di codice piu volte all'interno della nostra applicazione e questo ci è permesso tramite
- * delle speciali parole chiave.
- * Un'altra
  *
- * Nello specifico abbiamo 3 tipi fondamentali di cicli:
- * -FOR
- * -WHILE
- * -DO WHILE
+ * Che cosa sono i loop?
+ * I loop (o cicli) rispondono alla necessità di eseguire un blocco di codice più volte, a seconda del numero di iterazioni richieste.
+ * Spesso, nelle nostre applicazioni, dobbiamo ripetere una serie di operazioni: per esempio, leggere ogni singolo elemento di un array o
+ * elaborare una collezione di dati. I loop ci permettono di fare proprio questo, utilizzando delle parole chiave speciali per controllare
+ * l'esecuzione ripetuta del codice.
+ *
+ * I principali tipi di loop in JavaScript sono:
+ * - FOR: ideale quando conosciamo in anticipo il numero di iterazioni.
+ * - WHILE: esegue il blocco di codice finché una condizione risulta vera.
+ * - DO WHILE: esegue il codice almeno una volta, per poi continuare se la condizione è soddisfatta.
  *
  */
+
+/*
+  FOR
+
+  Il ciclo for in JavaScript permette di ripetere un blocco di codice un numero specifico di volte, grazie a tre elementi fondamentali:
+
+  - INDICE:
+    Una variabile (solitamente numerica) che tiene traccia dell'iterazione corrente. 
+    È particolarmente utile, ad esempio, per accedere agli elementi di un array.
+
+  - CONDIZIONE:
+    Un'espressione booleana che determina quando il ciclo deve terminare. 
+    Finché la condizione risulta vera, il ciclo continua; quando diventa falsa, il ciclo si ferma.
+
+  - ITERATORE:
+    L'istruzione che modifica l'indice ad ogni iterazione (ad esempio, incrementandolo) 
+    per avvicinarsi al punto in cui la condizione non sarà più soddisfatta.
+
+  Metafora del barman:
+  Immagina di essere un barman e di ricevere l'ordine per preparare 5 cocktail identici.
+  - La CONDIZIONE è "preparare 5 cocktail".
+  - L'INDICE rappresenta il numero di cocktail già preparati.
+  - L'ITERATORE è l'azione che incrementa il conteggio ad ogni cocktail completato.
+
+  In altre parole, il ciclo for ti aiuta a ripetere l'azione di preparare un cocktail, 
+  una volta per ogni cocktail richiesto, fino a completare l'ordine.
+*/
+
+for (let cocktail = 1; cocktail <= 5; cocktail++) {
+  console.log(`🍹 Cocktail numero ${cocktail} pronto!`);
+}
+
+/*
+  Ora applichiamo lo stesso concetto alla lettura degli elementi di un array.
+*/
+let array = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+/*
+  Cosa abbiamo fatto?
+
+  La variabile 'i' tiene traccia dell'indice corrente, ossia di dove siamo nell'array.
+  Ricorda che, come abbiamo visto, ogni elemento dell'array (la "scatolina") ha un'etichetta numerica,
+  e JavaScript inizia a numerarle da 0. Quindi, al primo giro del ciclo, 'i' vale 0 e accediamo al primo elemento.
+
+
+  La nostra condizione nel ciclo è che il numero di iterazioni deve essere MINORE del numero di elementi dell'array.
+  
+  Ma perché usiamo "minore" e non "minore o uguale"?
+  
+  - L'array.length ci dice quanti elementi sono presenti nell'array: nel nostro caso, 5.
+  - Ricorda che la numerazione degli indici inizia da 0. Quindi, in un array di 5 elementi, gli indici validi vanno da 0 a 4.
+  - Se usassimo la condizione "i <= array.length", l'ultima iterazione avrebbe i = 5, che non corrisponde a nessun elemento esistente.
+  - In questo caso, l'accesso array[5] restituirebbe "undefined", poiché non esiste un sesto elemento.
+
+  In sintesi, usando "i < array.length" garantiamo che il ciclo iteri solo sugli indici validi (da 0 a 4) e evitiamo errori.
+
+  Infine, abbiamo "i++". Come abbiamo già visto nel capitolo sugli operatori,
+  l'operatore incrementale (++) equivale a scrivere "i = i + 1". 
+  In altre parole, ad ogni iterazione del ciclo, incrementiamo il valore di "i" di 1.
+  
+  Questo incremento è fondamentale perché, ad ogni ciclo, spostiamo l'indice
+  verso il raggiungimento della condizione di terminazione (i < array.length).
+  
+  In questo esempio, incrementare di 1 è perfetto, poiché vogliamo leggere ogni singolo elemento dell'array.
+  Naturalmente, in altri scenari potremmo voler aumentare l'indice di un valore maggiore,
+  ma qui "i++" si adatta perfettamente alle nostre necessità.
+*/
+
+/*
+    Vediamo ancora un esempio per comprendere meglio il FOR
+*/
+
+for (let i = array.length - 1; i >= 0; i--) {
+  console.log(array[i]);
+}
+
+/*
+    In questo caso abbiamo completamente ribaltato la situazione.
+
+    Il CORPO del ciclo (ovvero la parte tra le parentesi graffe) è rimasto invariato,
+    e ciò che è cambiato sono i parametri del ciclo:
+
+    - Inizializzazione:  
+      Invece di iniziare da 0, la variabile "i" parte da "array.length - 1", che corrisponde all'ultimo indice dell'array.
+
+    - Condizione:  
+      Il ciclo continua finché "i" è maggiore o uguale a 0, assicurando che vengano processati tutti gli elementi fino al primo.
+
+    - Iteratore:  
+      Al posto di incrementare "i" con "i++", utilizziamo "i--" per decrementare il valore ad ogni iterazione,
+      così da spostarci all'indietro attraverso l'array.
+
+    In questo modo, il ciclo for itera l'array al contrario, partendo dall'ultimo elemento e procedendo verso il primo.
+*/
+
+/*
+    IN SINTESI, il ciclo for è composto da:
+    - Un indice: ci aiuta a tenere traccia del progresso verso il nostro obiettivo definito dalla condizione.
+    - Una condizione: stabilisce quando il ciclo deve terminare.
+    - Un iteratore: aggiorna l'indice ad ogni iterazione, permettendoci di progredire verso il raggiungimento della condizione.
+    - Il corpo del ciclo: il blocco di codice racchiuso tra parentesi graffe, dove eseguiamo le operazioni desiderate ad ogni iterazione.
+*/
+
+/*
+    CICLO WHILE
+    Il ciclo WHILE, a differenza del ciclo FOR non ha bisogno di tutti i parametri che abbiamo indicato nel ciclo FOR.
+    
+
+*/
---
+++ Javascript/06_classes&objects.js
@@ -0,0 +1,125 @@
+/*
+ * DISPENSA: CLASSI E OGGETTI IN JAVASCRIPT
+ *
+ * Una classe è un modello (o blueprint) che definisce le caratteristiche e le funzionalità
+ * comuni a un insieme di oggetti. In altre parole, la classe descrive sia la struttura (le proprietà)
+ * che i comportamenti (i metodi) che gli oggetti creati da essa avranno.
+ *
+ * Perché usare le classi?
+ * - Organizzare il codice: Raggruppare dati e funzioni correlate.
+ * - Riutilizzare il codice: Creare oggetti simili in modo efficiente.
+ * - Mantenere il codice chiaro: Separare la definizione degli oggetti dalla loro istanziazione.
+ *
+ * Analogia: Il Progetto Architettonico
+ * Immagina di avere il progetto per costruire una casa. Il progetto non è la casa stessa, ma contiene:
+ * - Proprietà: come l'indirizzo, il numero di stanze, il colore della facciata.
+ * - Metodi: le azioni che la casa può “eseguire” (in senso figurato), ad esempio "accendi le luci" o "apri la porta".
+ *
+ * In questo scenario:
+ * - Il progetto rappresenta la classe.
+ * - La casa costruita rappresenta un oggetto (o istanza) della classe.
+ *
+ * Proprietà vs. Metodi:
+ * - Proprietà: attributi o caratteristiche degli oggetti (es. indirizzo, numeroDiStanze).
+ * - Metodi: funzioni che definiscono i comportamenti degli oggetti (es. accendiLuci(), mostraDettagli()).
+ */
+
+/* Esempio Pratico in JavaScript */
+
+// Definizione della classe "Casa"
+class Casa {
+  // Il costruttore inizializza le proprietà dell'oggetto
+  constructor(indirizzo, numeroDiStanze) {
+    this.indirizzo = indirizzo;
+    this.numeroDiStanze = numeroDiStanze;
+  }
+
+  // Metodo che simula l'accensione delle luci
+  accendiLuci() {
+    console.log(`Luci accese in casa a ${this.indirizzo}`);
+  }
+
+  // Metodo che mostra i dettagli della casa
+  mostraDettagli() {
+    console.log(`Casa in ${this.indirizzo} con ${this.numeroDiStanze} stanze.`);
+  }
+}
+
+// Creazione di un oggetto "Casa" usando la classe definita
+const miaCasa = new Casa("Via Roma 10", 3);
+miaCasa.mostraDettagli(); // Output: Casa in Via Roma 10 con 3 stanze.
+miaCasa.accendiLuci(); // Output: Luci accese in casa a Via Roma 10
+
+/*
+  A livello tecnico, ecco cosa abbiamo implementato:
+
+  1. COSTRUTTORE:
+     - È una funzione speciale all'interno della classe.
+     - Viene eseguita automaticamente al momento della creazione di una nuova istanza.
+     - Permette di inizializzare le proprietà dell'oggetto, ricevendo in ingresso parametri che definiscono i valori iniziali.
+
+  2. PROPRIETÀ:
+     - Sono variabili associate alla classe e, di conseguenza, ad ogni istanza.
+     - Descrivono le caratteristiche dell'oggetto (ad esempio, indirizzo, numero di stanze, ecc.).
+
+  3. METODI:
+     - Sono funzioni definite all'interno della classe.
+     - Descrivono i comportamenti o le azioni che l'oggetto può eseguire.
+     - Vengono richiamati utilizzando le istanze della classe.
+     *NOTA: Dal momento che vengono definite all'interno di una classe non c'è bisogno della keyword FUNCTION, è implicita.
+
+  4. THIS:
+     - Rappresenta la classe corrente e permette di accedere a proprietà e metodi definiti all'interno della classe.
+*/
+/*
+  Esempio classe Persona
+
+  In questo esempio, la classe Persona:
+  - Ha un costruttore che inizializza le proprietà: nome, cognome ed eta.
+  - Definisce il metodo "saluta", che controlla in modo più semplice e leggibile 
+    se le proprietà "nome" o "cognome" sono valorizzate, utilizzando il controllo "truthy".
+*/
+
+class Persona {
+  constructor(nome, cognome, eta) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.eta = eta;
+  }
+
+  saluta() {
+    // Se "nome" è valorizzato (truthy), usalo per salutare
+    if (this.nome) {
+      console.log(`${this.nome} ti sta salutando`);
+    }
+    // Altrimenti, se "cognome" è valorizzato, usalo per salutare
+    else if (this.cognome) {
+      console.log(`${this.cognome} ti saluta`);
+    }
+    // Se nessuna delle due proprietà è disponibile, mostra un messaggio di default
+    else {
+      console.log("Una persona ignota ti sta salutando");
+    }
+  }
+}
+
+/* Esempio di utilizzo */
+
+// Caso in cui il nome è presente:
+const persona1 = new Persona("Marco", "Rossi", 25);
+persona1.saluta(); // Output: Marco ti sta salutando
+
+// Caso in cui il nome è una stringa vuota, quindi viene usato il cognome:
+const persona2 = new Persona("", "Verdi", 30);
+persona2.saluta(); // Output: Verdi ti saluta
+
+// Caso in cui né nome né cognome sono valorizzati:
+const persona3 = new Persona(null, null, 40);
+persona3.saluta(); // Output: Una persona ignota ti sta salutando
+
+// Possiamo accedere alle singole proprietà della classe tramite il '.'
+let nomePersona1 = persona1.nome;
+
+// Possiamo anche utilizzare il costruttore di "default" presente in ogni classe anche se non dichiarato
+const personaVuota = new Persona();
+console.log(personaVuota.nome); // Output: 'undefined' <-- se utilizziamo un costruttore di default le nostre proprietà avranno come valore 'undefined'
